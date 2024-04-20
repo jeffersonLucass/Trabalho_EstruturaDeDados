e3b5c8116832e8b5fc4c801ae7f3d33c91b575a7
jeffersonLucass <jeffersonlucas810@gmail.com>
2024-04-20 16:45:01 -0300

    codigofinalizado

--- main.c
+++ main.c
@@ -2,63 +2,77 @@
 #include <stdlib.h>
 #include <string.h>
 
-// Estrutura para armazenar informações do aluno
-struct Aluno {
+typedef struct
+{
     char nome[50];
-    float notas[3]; // Assumindo que há duas notas por aluno
-};
+    char telefone[20];
+    char curso[50];
+    float nota1;
+    float nota2;
+} Aluno;
 
-// Função para calcular a média das notas de um aluno
-float calcularMedia(float *notas) {
-    return (notas[0] + notas[1]) / 2.0;
+// Função para calcular a media de notas dos alunos.
+float calcularMedia(float nota1, float nota2)
+{
+    return (nota1 + nota2) / 2.0;
 }
 
-// Função para determinar a situação do aluno
-char *SituacaodoAluno(float media) {
-    if (media >= 7.0)
+// Função para determinar se o aluno esta aprovado ou reprovado baseado nas notas.
+char *SituacaoFinal(float media)
+{
+    if (media >= 7.0){
         return "APROVADO";
-    else
+    }    
+    else{
         return "REPROVADO";
+    }
 }
 
-int main() {
-    FILE *arquivoDeEntrada, *arquivoDeSaida;
-    char entrada[] = "DadosEntrada.csv.txt";
-    char saida[] = "SituacaoFinal.csv.txt";
-    char linha[100];
-    struct Aluno aluno;
+void CalculoFinal(Aluno *alunos, int TotalAlunos)
+{
+    FILE *arquivoSaida = fopen("SituacaoFinal.csv.txt", "w");
+    if (arquivoSaida == NULL)
+    {
+        fprintf(stderr, "Erro ao abrir o arquivo de saida.\n");
+        exit(1);
+    }
 
-    // Abrir arquivo de entrada
-    arquivoDeEntrada = fopen(entrada, "r");
-    if (arquivoDeEntrada == NULL) {
-        perror("Erro ao abrir o arquivo de entrada.\n");
+    // Modificação feita para não incluir na no arquivo final o curso e o telefone.
+    fprintf(arquivoSaida, "Nome,Nota Media,Situacao\n");
+
+    for (int i = 0; i < TotalAlunos; i++)
+    {
+        float media = calcularMedia(alunos[i].nota1, alunos[i].nota2);
+        const char *situacao = SituacaoFinal(media);
+        fprintf(arquivoSaida, "%s,%.2f,%s\n", alunos[i].nome, media, situacao);
+    }
+
+    fclose(arquivoSaida);
+}
+
+int main()
+{
+    FILE *arquivoEntrada;
+    Aluno alunos[100]; // total de alunos.
+    int TotalAlunos = 0;
+
+    arquivoEntrada = fopen("DadosEntrada.csv.txt", "r");
+    if (arquivoEntrada == NULL)
+    {
+        fprintf(stderr, "Erro ao abrir o arquivo de entrada.\n");
         return 1;
     }
 
-    // Abrir arquivo de saída
-    arquivoDeSaida = fopen(saida, "w");
-    if (arquivoDeSaida == NULL) {
-        perror("Erro ao abrir o arquivo de saída.\n");
-        fclose(arquivoDeEntrada);
-        return 1;
+    while (fscanf(arquivoEntrada, "%[^,],%[^,],%[^,],%f,%f\n", alunos[TotalAlunos].nome, alunos[TotalAlunos].telefone, alunos[TotalAlunos].curso, &alunos[TotalAlunos].nota1, &alunos[TotalAlunos].nota2) != EOF)
+    {
+        TotalAlunos++;
     }
 
-    // Ler dados do arquivo de entrada, calcular média e escrever no arquivo de saída
-    while (fgets(linha, sizeof(linha), arquivoDeEntrada) != NULL) {
-        sscanf(linha, "%[^,],%f,%f", aluno.nome, &aluno.notas[0], &aluno.notas[1]);
-        
-        float media = calcularMedia(aluno.notas);
-        char *situacaoFinal = SituacaodoAluno(media);
-        
-        // Escrever no arquivo de saída
-        fprintf(arquivoDeSaida, "%s,%.2f,%s\n", aluno.nome, media,situacaoFinal );
-    }
+    fclose(arquivoEntrada);
 
-    // Fechar arquivos
-    fclose(arquivoDeEntrada);
-    fclose(arquivoDeSaida);
+    CalculoFinal(alunos, TotalAlunos);
 
-    printf("Processo concluido. Verifique o arquivo SituacaoFinal.csv.\n");
+    printf("O Arquivo SituacaoFinal.csv foi criado com sucesso.\n");
 
     return 0;
 }
